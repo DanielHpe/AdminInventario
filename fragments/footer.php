ea4d91a04f068c8828f07e7bf823467a053a4f29
Danielhpe <danihpe@hotmail.com>
2019-04-03 11:50:40 -0300

    Adição da Biblioteca datatable

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,5 +1,6 @@
 <script src="lib/js/jquery.min.js"></script>
 <script src="lib/js/jquery-pack.js"></script>
 <script src="lib/js/jquery-masks.js"></script>
+<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="view/js/jquery.js"></script>
 </html>
