06fdd709f38f3cb2c9dc0a3b2a1068d91e758685
Danielhpe <danihpe@hotmail.com>
2019-04-03 11:27:48 -0300

    Adição do Header e o Footer

---
+++ fragments/footer.php
@@ -0,0 +1,5 @@
+<script src="lib/js/jquery.min.js"></script>
+<script src="lib/js/jquery-pack.js"></script>
+<script src="lib/js/jquery-masks.js"></script>
+<script src="view/js/jquery.js"></script>
+</html>
