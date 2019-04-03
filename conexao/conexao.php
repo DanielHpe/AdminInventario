250196e94dac3ac3e05505571bf18120579eb6c3
Danielhpe <danihpe@hotmail.com>
2019-04-03 11:27:31 -0300

    Conexão com o banco de dados

---
+++ conexao/conexao.php
@@ -0,0 +1,49 @@
+<?php 
+
+class Conexao{
+
+    public $conn;
+    public $hostname = 'localhost';
+    public $database = 'systeminfotest';
+    public $charset = "utf8";
+    public $user = "root";
+    public $password = "";
+    public $tablename = "userpcinfo";
+
+	public function __construct(){
+        $this->conn = new PDO (
+            "mysql:host=$this->hostname; 
+            dbname=$this->database; 
+            charset=$this->charset",
+            $this->user,
+            $this->password
+        );
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+	public function query($string_query){
+		$this->conn->exec($string_query);
+    }
+
+    public function select($string_query, $param){
+        $stmt = $this->conn->prepare($string_query); 
+        $stmt->bindParam(1, $param, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC); 
+        return $result;
+    }
+    
+    public function getTableName(){
+        return $this->tablename;
+    }
+
+    public function setTableName($tablename){
+        $this->tablename = $tablename;
+    }
+    
+    public function __destruct(){
+        $this->conn = null;
+    }
+}
+
+?>
